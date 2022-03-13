fef1ff0ec64a2dedd03689eb9c82d7ba39626f79
Denis <denis.boboruchin@gmail.com>
2022-03-14 00:26:28 +0300

    first programs

---
+++ les1/1.asm
@@ -0,0 +1,26 @@
+global _start
+
+section .text
+
+_start:
+    mov     eax, 3
+    mov     ebx, 2
+    mov     ecx, c
+    mov     edx, 1
+
+    int     0x80
+
+    mov     eax, 4
+    mov     ebx, 1
+    mov     ecx, c
+    mov     edx, 1
+
+    int     0x80
+
+    mov     eax, 1
+    mov     ebx, 0
+
+    int     0x80
+
+section .bss
+    c resb 1
