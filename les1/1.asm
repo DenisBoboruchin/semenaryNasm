55a1e2b30975003958ae956d833caa2c82a33ce8
Denis <denis.boboruchin@gmail.com>
2022-03-14 19:11:19 +0300

    3 sem

--- les1/1.asm
+++ les1/1.asm
@@ -3,24 +3,24 @@ global _start
 section .text
 
 _start:
-    mov     eax, 3
-    mov     ebx, 2
-    mov     ecx, c
-    mov     edx, 1
+            mov     eax, 3
+            mov     ebx, 2
+            mov     ecx, c
+            mov     edx, 1
 
-    int     0x80
+            int     0x80
 
-    mov     eax, 4
-    mov     ebx, 1
-    mov     ecx, c
-    mov     edx, 1
+            mov     eax, 4
+            mov     ebx, 1
+            mov     ecx, c
+            mov     edx, 1
 
-    int     0x80
+            int     0x80
 
-    mov     eax, 1
-    mov     ebx, 0
+            mov     eax, 1
+            mov     ebx, 0
 
-    int     0x80
+            int     0x80
 
 section .bss
-    c resb 1
+            c resb 1
